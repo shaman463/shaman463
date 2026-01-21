4227e1c4200eb639b602ddf26febaf43b878854d
Shamandeep Singh <ssingh1_mca24@thapar.edu>
2026-01-21 15:21:54 +0530

    Delete src directory

--- src/utils/debug.js
+++
@@ -1,7 +0,0 @@
-// Debug utility to check API URL
-console.log('🔍 Debug Info:');
-console.log('Environment:', import.meta.env.MODE);
-console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
-console.log('Final API URL:', import.meta.env.VITE_API_URL || 'http://localhost:5000');
-
-export default {};
